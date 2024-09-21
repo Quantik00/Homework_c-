6caec9a361d37342933624117959da643fac59d5
Sveta <145128663+Quantik00@users.noreply.github.com>
2024-09-21 23:28:00 +0300

    Add files via upload

---
+++ privet_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rep;
+    cout << "Чиселко пожалуйста" << endl;
+    cin >> rep;
+
+    for(int i = 0; i < rep; i++)
+    {
+        cout << "Привет мир!" << endl;
+    }
+
+    return 0;
+}
